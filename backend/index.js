1faaae634e7cd1a6ae932183873d3702e4360a84
Chaudhary0501 <chhayachaudhary1212@gmail.com>
2025-02-15 11:57:57 +0530

    changed cors

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://collab-at.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
